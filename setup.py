10854021d71d965dc25091b6a0b2eaa2cae8abc3
Hsin-Yi Chen (hychen) <ossug.hychen@gmail.com>
2010-08-01 16:09:55 +0800

    * add missing dependency.

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from distutils.core import setup
 
 setup(
     name = 'vsgui',
+    requires = ['ucltip(>=0.2)'],
     version = open('VERSION.txt').read().strip(),
     description = 'Very Simple Graphical library for Python shell script',
     long_description=open('README.txt').read(),
@@ -13,5 +14,4 @@ setup(
     author_email = 'ossug.hychen@gmail.com',
     license = 'GPLv2',
     packages=['vsgui'],
-    zip_safe=False,
 )
